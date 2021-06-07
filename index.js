69d3f90398cc8ae4517a4c5653af18c00907a9ec
Shirley Gonzales <81574751+Shian2709@users.noreply.github.com>
2021-06-07 16:05:41 -0500

    07-06-21

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function mostrarMensaje(){
-    alert("Hola amiguitos")
+    alert("Holi, profe")
 }
+let numero4=prompt("Ingresa el primero numero")
+let numero5=prompt("Ingresa el segundo numero")
+
+numero4=parseInt(numero4)
+numero5=parseInt(numero5)
